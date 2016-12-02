6f9e970799168386649022351e0c97a3695ddbb2
Tomas Knap <toknap@gmail.com>
2016-12-02 13:59:20 +0100

    Added voc file

---
+++ e-httpRequest/src/main/java/eu/unifiedviews/plugins/extractor/httprequest/HttpRequestVocabulary.java
@@ -0,0 +1,15 @@
+package eu.unifiedviews.plugins.extractor.httprequest;
+
+/**
+ * Example:
+ * <http://localhost/resource/config>
+ * <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://unifiedviews.eu/ontology/dpu/httpRequest/Config>;
+ * <http://unifiedviews.eu/ontology/dpu/httpRequest/requestBody> """{"pageUrl" : "http://mytestt3.com","title" : "testt title updated", "username" : "test user", "spaceKey" : "test space", "text" : "testt text updated", "creationDate" : "2016-11-12" }""" .
+ */
+public class HttpRequestVocabulary {
+
+    public static final String PREFIX = "http://unifiedviews.eu/ontology/dpu/httpRequest/";
+    public static final String STR_CONFIG_CLASS = PREFIX + "Config";
+    public static final String STR_RAW_REQUEST_BODY = PREFIX + "requestBody";
+
+}
